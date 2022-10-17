02d1f9055c7685ff38cf40c635fdda956c5b3ba0
Deepak Basavaraju <deepakcool208+git@gmail.com>
2022-10-18 02:07:56 +0530

    new end screen code half done, not fixed completely

--- games/sweet-memory-game/script.js
+++ games/sweet-memory-game/script.js
@@ -63,20 +63,34 @@ function endGame(moves, score) {
 	var total = allContent.responseJSON["content"].length,
 			number = Math.floor(Math.random() * total);
 	$deck.empty();
-	swal({
-		allowEscapeKey: false,
-		allowOutsideClick: false,
-		title: 'Congratulations!',
-		html: 'You won with ' + moves + ' Moves' + 
-		'<br/><div>' + allContent.responseJSON["content"][number]["text"] + '</div>',
-		type: 'success',
-		confirmButtonColor: '#9BCB3C',
-		confirmButtonText: 'Play again!'
-	}).then(function(isConfirm) {
-		if (isConfirm) {
-			initGame();
-		}
-	})
+	showEndScreen(moves);
+	// swal({
+	// 	allowEscapeKey: false,
+	// 	allowOutsideClick: false,
+	// 	title: 'Congratulations!',
+	// 	html: 'You won with ' + moves + ' Moves' + 
+	// 	'<br/><div>' + allContent.responseJSON["content"][number]["text"] + '</div>',
+	// 	type: 'success',
+	// 	showDenyButton: true,
+ //    showCancelButton: true,
+ //    confirmButtonText: '<i class="fa fa-repeat fa-2x" aria-hidden="true"></i>',
+ //    denyButtonText: '<i class="fa fa-random fa-2x" aria-hidden="true"></i>',
+ //    cancelButtonText: '<i class="fa fa-times fa-2x" aria-hidden="true"></i>',    
+	// 	// confirmButtonColor: '#9BCB3C',
+	// 	// confirmButtonText: 'Play again!'
+	// }).then((result) => {
+	// 	console.log(result);
+ //    if (result.isConfirmed) {
+ //        initGame();
+ //    } else if (result.isDenied) {
+ //        window.location.href = config['prod_url'];
+ //    } else if (result.dismiss === Swal.DismissReason.cancel) {
+ //        window.location.href = config['prod_url'];
+ //    }		
+	// 	// if (isConfirm) {
+	// 	// 	initGame();
+	// 	// }
+	// })
 }
 
 // Restart Game
@@ -140,4 +154,70 @@ $deck.on('click', '.card:not(".match, .open")', function() {
   }
 });
 
-initGame();
+// initGame();
+
+function get_content() {
+	var total = allContent.responseJSON["content"].length,
+			number = Math.floor(Math.random() * total);
+	return allContent.responseJSON["content"][number]["text"];	
+}
+
+function share() {
+  if (navigator.share) {
+    navigator.share({
+      title: 'Chill Panda',
+      url: window.location.href
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    }).catch(err => {
+      console.log('Error while using Web share API:');
+      console.log(err);
+    });
+  } else {
+    swal("Browser doesn't support this API !");
+  }	
+}
+
+showEndScreen();
+
+function showEndScreen(moves) {
+	var allContent = $.getJSON('../../resources/content.json');
+  swal({
+    allowEscapeKey: false,
+    allowOutsideClick: false,
+    title: 'Congratulations!',
+    html: '<span>You won with <strong>' + moves + '</strong> Moves </span>',
+    type: 'success',
+    backdrop: 'white',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: '<i class="fa fa-repeat fa-2x" aria-hidden="true"></i>',
+    denyButtonText: '<i class="fa fa-random fa-2x" aria-hidden="true"></i>',
+    cancelButtonText: '<i class="fa fa-times fa-2x" aria-hidden="true"></i>',
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+        resetGame();
+    } else if (result.isDenied) {
+        window.location.href = config['prod_url'];
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+        window.location.href = config['prod_url'];
+    }
+  });
+  var triggerDiv = '<div class="trigger-div">' + 'get_content()' + '</div>';
+  $('.swal2-container').append(triggerDiv);
+  var shareDiv = document.createElement('div');
+  shareDiv.className = 'share-div';
+  shareDiv.innerHTML = '<i class="fa fa-share fa-2x" aria-hidden="true"></i>';
+  shareDiv.addEventListener('click', share);
+  $('.swal2-container').append(shareDiv);
+  var buttonTextDiv = document.createElement('div');
+  buttonTextDiv.className = 'button-div';
+  buttonTextDiv.innerHTML = '<span>Repeat</span><span>Shuffle</span><span>Exit</span>';
+  $('.swal2-container').append(buttonTextDiv);
+  var logoDiv = document.createElement('div');
+  logoDiv.className = 'logo-div';
+  logoDiv.innerHTML = '<a href="http://coffeecrush.net/site/" target="_blank">' 
+  + '<img src="../../assets/CoffeeCrush.jpg">' + '</a>';
+  $('.swal2-container').append(logoDiv);
+}
