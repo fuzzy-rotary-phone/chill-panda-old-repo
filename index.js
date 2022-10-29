45924363c33b51573511b75067f0071a2fc4bd04
vamsy19 <r.vamsy19@gmail.com>
2022-10-29 21:50:36 +0530

    Added Firebase Analytics to the Chill Panda Site

--- index.js
+++ index.js
@@ -27,3 +27,25 @@ function loadGame() {
 	var number = getRandomNumber();
 	$('#myiframe').attr('src', gameMap[number]);
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJQO8HWWCV6a-K33knwHYnIcZwm5-4G9w",
+  authDomain: "chill-panda-c1184.firebaseapp.com",
+  projectId: "chill-panda-c1184",
+  storageBucket: "chill-panda-c1184.appspot.com",
+  messagingSenderId: "97999123599",
+  appId: "1:97999123599:web:c214990d78ab04860b6cfa",
+  measurementId: "G-QNEMSD28TV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
