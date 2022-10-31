4a8bfefc3bd274bbe20d8937e1ac1f5b3c1ffbd4
Deepak Basavaraju <deepakcool208+git@gmail.com>
2022-10-31 18:51:12 +0530

    rating some fixes

--- rating.js
+++ rating.js
@@ -5,18 +5,22 @@ function storeRating(stars) {
 
 	db.collection("rating").add({
 	    guid: localStorage['guid'] ? localStorage['guid'] : null,
-	    game_id: localStorage['lastGame'] ? localStorage['lastGame'] : null,
+	    game_id: localStorage['lastGame'] ? parseInt(localStorage['lastGame']) : null,
 	    rating: stars,
-	    created_at: new Date(),
+	    created_at: Math.round(Date.now() / 1000),
 	    hostname: window.location.hostname
 	})
 	.then((docRef) => {
 	    console.log("Document written with ID: ", docRef.id);
+	    home();
 	})
 	.catch((error) => {
 	    console.error("Error adding document: ", error);
+	    home();
 	});
+}
 
+function home() {
 	window.location.href = window.location.origin;
 }
 
